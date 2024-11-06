35c66f41a38a27927e3504a16eb2b73c8a23a6e4
ITSTART <itstart@step.kz>
2024-11-06 17:13:49 +0500

    lesson_05 files

---
+++ testapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class AdvUser(models.Model):
+    is_activate = models.BooleanField(default=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Spare(models.Model):
+    name = models.CharField(max_length=30)
+
+class Machine(models.Model):
+    name = models.CharField(max_length=30)
+    spares = models.ManyToManyField(Spare)
